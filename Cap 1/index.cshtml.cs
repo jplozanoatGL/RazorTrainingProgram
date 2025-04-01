9d67c370f785555773ae8b4fe73dfcd01e541dcf
jplozanoatGL <juanpablo.lozanoarce@globallogic.com>
2025-04-01 15:51:07 -0300

    Capitulo 2 en progreso

---
+++ Cap 1/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class IndexModel : PageModel
+{
+    public string Message { get; private set; } = "World";
+
+    public void OnGet()
+    {
+        // TODO: Change the message to "Tech Enthusiast"
+    }
+}
---
+++ Cap 2/BlogPost.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class BlogPost
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
